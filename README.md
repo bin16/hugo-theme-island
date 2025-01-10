d928aec12fe3259c247da424edad78e330acb0e2
bin16 <11701531+bin16@users.noreply.github.com>
2025-01-10 21:19:06 +0800

    clear packages

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-assets/css/tailwind.css -> assets/css/main.css
+Yet another hugo theme, based on Tailwind CSS 4.0, work in progress.
--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    plugins: {
-        tailwindcss: {
-            // config: './tailwindcss-config.js',
-        },
-    }
-};
